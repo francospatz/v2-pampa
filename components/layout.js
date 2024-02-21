b16cdb00f62228348afa78cc4a30646becb31aa6
francospatz <francospatzdilorenzo@gmail.com>
2024-02-21 17:35:47 +0100

    working on window size

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,5 @@
-import Div100vh from "react-div-100vh"
+
 
 export default function Layout({ children }) {
-  return (
-    <Div100vh>
-      {children}
-    </Div100vh>
-  )
+  return (<>{children}</>)
 }
