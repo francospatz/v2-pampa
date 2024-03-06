1b5fd7b8f8d2c527b0254c57ef16e41a09c09c53
francospatz <francospatzdilorenzo@gmail.com>
2024-03-06 10:08:57 +0100

    small fixes

--- pages/restaurant/components/gallery.js
+++ pages/restaurant/components/gallery.js
@@ -39,81 +39,81 @@ export default function Gallery() {
 
     return (
         <Layout >
-            <ReactLenis ref={lenisRef}>
-                <NextSeo title="Gallery" />
-                <LazyMotion features={domAnimation}>
-                    <m.div
-                        initial="initial"
-                        animate="enter"
-                        exit="exit"
-                    >
-                        <Div100vh className='w-full bg-off-white'>
-                            <m.div variants={fade} className="h-full bg-off-white text-light-brown selection:bg-off-white selection:text-light-brown">
+            {/* <ReactLenis ref={lenisRef}> */}
+            <NextSeo title="Gallery" />
+            <LazyMotion features={domAnimation}>
+                <m.div
+                    initial="initial"
+                    animate="enter"
+                    exit="exit"
+                >
+                    <Div100vh className='w-full bg-off-white'>
+                        <m.div variants={fade} className="h-full bg-off-white text-light-brown selection:bg-off-white selection:text-light-brown">
 
-                                <m.main variants={fade} className="pb-0 p-4 md:p-6 relative w-full relative bg-off-white h-[85%] md:h-[91%] flex  flex-col items-start justify-start" >
-                                    <div className="w-full h-[8%] relative z-10  flex justify-center items-center">
+                            <m.main variants={fade} className="pb-0 p-4 md:p-6 relative w-full relative bg-off-white h-[85%] md:h-[91%] flex  flex-col items-start justify-start" >
+                                <div className="w-full h-[8%] relative z-10  flex justify-center items-center">
 
-                                        <p className="w-full md:w-11/12 font-serif text-[3rem] md:text-[4rem] text-center font-bold" >
-                                            {langContext === 'es' ? 'GALERÍA' : 'GALLERY'}
-                                        </p>
+                                    <p className="w-full md:w-11/12 font-serif text-[3rem] md:text-[4rem] text-center font-bold" >
+                                        {langContext === 'es' ? 'GALERÍA' : 'GALLERY'}
+                                    </p>
 
-                                    </div>
+                                </div>
 
-                                    <article className="flex w-full justify-center items-start h-[92%]">
+                                <article className="flex w-full justify-center items-start h-[92%]">
 
-                                        <m.div variants={fade} className="w-full font-serif text-xl xl:text-[22px] leading-tight tracking-tight content flex flex-col justify-center items-center h-full">
+                                    <m.div variants={fade} className="w-full font-serif text-xl xl:text-[22px] leading-tight tracking-tight content flex flex-col justify-center items-center h-full">
 
-                                            {/* <div className='w-full h-full flex flex-col items-center justify-center'> */}
-                                            <div className='font-serif leading-none tracking-tight h-full w-full flex justify-around items-center'>
-                                                <ImageCarousel images={images}></ImageCarousel>
-                                            </div>
-                                            {/* </div> */}
-
-
-
-                                        </m.div>
-
-                                    </article>
-
-                                </m.main>
-                                <m.footer variants={fade} className="pt-0 h-[15%] md:h-[9%] p-4 md:p-6 bg-off-white 
-                                 w-full flex items-end">
-                                    <div className="flex flex-col md:flex-row w-full">
-
-                                        <span className="block w-auto font-sans uppercase text-sm  xl:text-lg font-normal leading-[1.15] md:leading-[1.15] tracking-tight mr-8 mb-[5px] md:mb-0">© Pampa Grill 2024</span>
-
-
-
-                                        <div className='flex flex-row md:mr-0 md:ml-auto'>
-
-
-                                            <span className="block w-auto font-sans uppercase text-sm  xl:text-lg font-normal leading-[1.15] md:leading-[1.15] tracking-tight mr-8 mb-[5px] md:mb-0">
-                                                <a href="https://www.thefork.es/restaurante/pampa-grill-malaga-r363341" target='_blank' className="group relative">
-                                                    {langContext === 'es' ? 'Reservas' : 'Bookings'}
-                                                    <span className="absolute bottom-0 left-0 right-0 border-b border-current w-full group-hover:w-0 group-focus:w-0 transition-all ease-in-out duration-500 mb-[-1px] md:mb-[-3px]"></span></a>
-                                            </span>
-                                            <span className="block w-auto font-sans uppercase text-sm  xl:text-lg font-normal leading-[1.15] md:leading-[1.15] tracking-tight mr-8 mb-[5px] md:mb-0">
-                                                <Link legacyBehavior href="/contact"><a className="group relative">{langContext === 'es' ? 'Contacto' : 'Contact'}  <span className="absolute bottom-0 left-0 right-0 border-b border-current w-full group-hover:w-0 group-focus:w-0 transition-all ease-in-out duration-500 mb-[-1px] md:mb-[-3px]"></span></a></Link>
-                                            </span>
-                                            <span className="block w-1/2  text-right  ml-auto font-sans uppercase text-sm md:text-base xl:text-lg font-normal leading-[1.15] md:leading-[1.15] tracking-tight mb-[5px] md:mb-0">
-                                                <Link legacyBehavior href="/restaurant"><a className="group relative">
-                                                    {langContext === 'es' ? 'Volver' : 'Back'}
-                                                    <span className="absolute bottom-0 left-0 right-0 border-b border-current w-full group-hover:w-0 group-focus:w-0 transition-all ease-in-out duration-500 mb-[-1px] md:mb-[-3px]"></span></a></Link>
-                                            </span>
-
-
+                                        {/* <div className='w-full h-full flex flex-col items-center justify-center'> */}
+                                        <div className='font-serif leading-none tracking-tight h-full w-full flex justify-around items-center'>
+                                            <ImageCarousel images={images}></ImageCarousel>
                                         </div>
+                                        {/* </div> */}
+
+
+
+                                    </m.div>
+
+                                </article>
+
+                            </m.main>
+                            <m.footer variants={fade} className="pt-0 h-[15%] md:h-[9%] p-4 md:p-6 bg-off-white 
+                                 w-full flex items-end">
+                                <div className="flex flex-col md:flex-row w-full">
+
+                                    <span className="block w-auto font-sans uppercase text-sm  xl:text-lg font-normal leading-[1.15] md:leading-[1.15] tracking-tight mr-8 mb-[5px] md:mb-0">© Pampa Grill 2024</span>
+
+
+
+                                    <div className='flex flex-row md:mr-0 md:ml-auto'>
+
+
+                                        <span className="block w-auto font-sans uppercase text-sm  xl:text-lg font-normal leading-[1.15] md:leading-[1.15] tracking-tight mr-8 mb-[5px] md:mb-0">
+                                            <a href="https://www.thefork.es/restaurante/pampa-grill-malaga-r363341" target='_blank' className="group relative">
+                                                {langContext === 'es' ? 'Reservas' : 'Bookings'}
+                                                <span className="absolute bottom-0 left-0 right-0 border-b border-current w-full group-hover:w-0 group-focus:w-0 transition-all ease-in-out duration-500 mb-[-1px] md:mb-[-3px]"></span></a>
+                                        </span>
+                                        <span className="block w-auto font-sans uppercase text-sm  xl:text-lg font-normal leading-[1.15] md:leading-[1.15] tracking-tight mr-8 mb-[5px] md:mb-0">
+                                            <Link legacyBehavior href="/contact"><a className="group relative">{langContext === 'es' ? 'Contacto' : 'Contact'}  <span className="absolute bottom-0 left-0 right-0 border-b border-current w-full group-hover:w-0 group-focus:w-0 transition-all ease-in-out duration-500 mb-[-1px] md:mb-[-3px]"></span></a></Link>
+                                        </span>
+                                        <span className="block w-1/2  text-right  ml-auto font-sans uppercase text-sm md:text-base xl:text-lg font-normal leading-[1.15] md:leading-[1.15] tracking-tight mb-[5px] md:mb-0">
+                                            <Link legacyBehavior href="/restaurant"><a className="group relative">
+                                                {langContext === 'es' ? 'Volver' : 'Back'}
+                                                <span className="absolute bottom-0 left-0 right-0 border-b border-current w-full group-hover:w-0 group-focus:w-0 transition-all ease-in-out duration-500 mb-[-1px] md:mb-[-3px]"></span></a></Link>
+                                        </span>
+
+
                                     </div>
-                                </m.footer>
+                                </div>
+                            </m.footer>
 
 
-                            </m.div>
+                        </m.div>
 
-                        </Div100vh>
+                    </Div100vh>
 
-                    </m.div>
-                </LazyMotion>
-            </ReactLenis>
+                </m.div>
+            </LazyMotion>
+            {/* </ReactLenis> */}
         </Layout>
     )
 }
