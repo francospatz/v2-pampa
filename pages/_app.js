27ee46b8c64488071c150a5ac0bd4bb606a52dd3
francospatz <francospatzdilorenzo@gmail.com>
2024-03-06 04:58:57 +0100

    small fixes

--- pages/_app.js
+++ pages/_app.js
@@ -117,8 +117,8 @@ export default function App({ Component, pageProps }) {
                           src={image1}
                           className="scale-105 absolute inset-0 object-cover object-center h-full w-full"
                           priority={true}
-                          width={4}
-                          height={6}
+                          width={1334}
+                          height={1613}
                         />
                       </div>
 
