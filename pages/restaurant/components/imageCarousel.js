bbeca9e81d57bc788caaf7d83d741813a670684a
francospatz <francospatzdilorenzo@gmail.com>
2024-03-06 03:31:16 +0100

    added images to the gallery

--- pages/restaurant/components/imageCarousel.js
+++ pages/restaurant/components/imageCarousel.js
@@ -9,24 +9,80 @@ import { fadeLate } from '@/helpers/transitions';
 
 
 const images = [
-    { width: 400, height: 600, src: '/images/gallery/food/1.webp' },
-    { width: 400, height: 600, src: '/images/gallery/food/2.webp' },
-    { width: 400, height: 600, src: '/images/gallery/food/3.webp' },
-    { width: 400, height: 600, src: '/images/gallery/food/4.webp' },
-    { width: 400, height: 600, src: '/images/gallery/food/5.webp' },
-    { width: 400, height: 600, src: '/images/gallery/food/6.webp' },
-    { width: 400, height: 600, src: '/images/gallery/food/7.webp' },
-    { width: 400, height: 600, src: '/images/gallery/food/8.webp' },
-    { width: 400, height: 600, src: '/images/gallery/restaurant/1.webp' },
-    { width: 400, height: 600, src: '/images/gallery/restaurant/2.webp' },
-    { width: 400, height: 600, src: '/images/gallery/restaurant/3.webp' },
-    { width: 400, height: 600, src: '/images/gallery/restaurant/4.webp' },
-    { width: 400, height: 600, src: '/images/gallery/restaurant/5.webp' },
-    { width: 600, height: 400, src: '/images/gallery/restaurant/6.webp' },
-    { width: 600, height: 400, src: '/images/gallery/restaurant/7.webp' },
-    { width: 600, height: 400, src: '/images/gallery/restaurant/8.webp' },
-
-
+    { width: 4, height: 6, src: '/images/gallery/food/1.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/2.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/3.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/4.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/5.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/6.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/7.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/8.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/9.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/10.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/11.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/12.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/13.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/14.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/15.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/16.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/17.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/18.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/19.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/20.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/21.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/22.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/23.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/24.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/25.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/26.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/27.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/28.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/29.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/30.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/32.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/33.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/34.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/35.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/36.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/37.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/38.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/39.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/40.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/41.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/42.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/43.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/44.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/45.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/46.webp' },
+    { width: 6, height: 4, src: '/images/gallery/food/47.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/48.webp' },
+    { width: 4, height: 6, src: '/images/gallery/food/49.webp' },
+    { width: 4, height: 6, src: '/images/gallery/restaurant/1.webp' },
+    { width: 4, height: 6, src: '/images/gallery/restaurant/2.webp' },
+    { width: 6, height: 4, src: '/images/gallery/restaurant/3.webp' },
+    { width: 4, height: 6, src: '/images/gallery/restaurant/4.webp' },
+    { width: 4, height: 6, src: '/images/gallery/restaurant/5.webp' },
+    { width: 6, height: 4, src: '/images/gallery/restaurant/6.webp' },
+    { width: 6, height: 4, src: '/images/gallery/restaurant/7.webp' },
+    { width: 6, height: 4, src: '/images/gallery/restaurant/8.webp' },
+    { width: 6, height: 4, src: '/images/gallery/restaurant/9.webp' },
+    { width: 6, height: 4, src: '/images/gallery/restaurant/10.webp' },
+    { width: 6, height: 4, src: '/images/gallery/restaurant/11.webp' },
+    { width: 4, height: 6, src: '/images/gallery/restaurant/12.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/1.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/2.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/3.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/4.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/5.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/6.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/7.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/8.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/9.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/10.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/11.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/12.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/13.webp' },
+    { width: 4, height: 6, src: '/images/gallery/team/14.webp' },
 ];
 
 
@@ -36,9 +92,13 @@ export default function ImageGallery() {
         swiperRef.current.swiper.slideTo(i);
     };
 
-    /* const handleThumbnailClick = (index) => {
-        swiperRef.current.toswiper.
-    } */
+    const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
 
     return (
         <>
