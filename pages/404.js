9a97466d9b41690a0faba5cad5159b092f32989d
francospatz <francospatzdilorenzo@gmail.com>
2024-03-13 13:00:50 +0100

    changed 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+'use client'
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+export default function Custom404() {
+    const router = useRouter()
+    useEffect(() => {
+        router.replace('/')
+    }, [])
+
+    return <></>
+}
